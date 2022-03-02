de7d8c1672fdfd21ce0629a1719ab51cb4bdfdd0
Sebastian <sebastian@iamsebastian.dev>
2022-03-02 12:27:14 +0100

    FIX: Fixed wrong typename

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 /** @format */
 
 export { persistable } from './stores/persistable';
-export { reduceable, Reduceable, Dispatch } from './stores/reduceable';
+export { reduceable, Reduceable, Action } from './stores/reduceable';
