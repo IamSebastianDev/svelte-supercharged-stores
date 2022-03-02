4da13124233bc9503861847a0d7e898ff8c255f5
Sebastian <sebastian@iamsebastian.dev>
2022-03-02 12:28:34 +0100

    FEAT: Added a persistable store that will keep it's value in sync with the local or session storeage

--- stores/persistable.ts
+++ stores/persistable.ts
@@ -1,3 +1,36 @@
 /** @format */
 
-export const persistable = () => {};
+import { assertStore } from '../internal/assertStore';
+import { Writable, writable } from 'svelte/store';
+
+interface StoreInit {
+	namespace?: string;
+	method: 'localStorage' | 'sessionStorage';
+}
+
+export const persistable = (
+	argument: any | Writable<any>,
+	identifier: string,
+	storeInit: StoreInit = {
+		method: 'localStorage',
+	}
+): Writable<any> => {
+	let store;
+	const { method, namespace } = storeInit;
+	const storageIdentifier = (namespace ? namespace + '__' : '') + identifier;
+	let storedValue = window[method].getItem(storageIdentifier);
+	storedValue && JSON.parse(storedValue);
+
+	if (assertStore(argument)) {
+		store = argument;
+		store.set(storedValue);
+	} else {
+		store = writable(storedValue || argument);
+	}
+
+	store.subscribe((state: any) =>
+		window[method].setItem(storageIdentifier, JSON.stringify(state))
+	);
+
+	return store;
+};
