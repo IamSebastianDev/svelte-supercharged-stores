dbd3e8ae0116caf3dede709c01a2a6ccb8637208
Sebastian <sebastian@iamsebastian.dev>
2022-03-28 22:14:17 +0200

    FIX: Fixed an issue that would lead to a corrupt store

--- stores/persistable.ts
+++ stores/persistable.ts
@@ -15,7 +15,7 @@ interface StoreInit {
  * provided as argument. If a store is passed as an argument, the store itself is instead set to the value.
  * This means svelte custom stores can be persistet easily.
  *
- * @param { Writable<any> } argument - the value/store that is used to initalize the persistable.
+ * @param { any } argument - the value that is used to initalize the persistable.
  * @param { string } identifier - the identifier used to describe the created store. This value should be unique, but
  * **consistent** between sessions.
  * @param { {} } storeInit - a configuration object used to configure the stores behaviour.
@@ -29,12 +29,12 @@ interface StoreInit {
  */
 
 export const persistable = <T>(
-	argument: Writable<T>,
+	argument: T,
 	identifier: string,
 	storeInit: StoreInit = {
 		method: 'localStorage',
 	}
-): Writable<T> => {
+): Persistable<T> => {
 	const { method, namespace } = storeInit;
 	const storageIdentifier = (namespace ? namespace + '__' : '') + identifier;
 	let storedValue = window[method].getItem(storageIdentifier);
